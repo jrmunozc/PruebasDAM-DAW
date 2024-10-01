def97a877e3b367a2bb494b9f228cd96eb3e5bc3
alejandrodenova <adenovaf@alumnes.santjosepobrer.es>
2024-10-01 17:11:59 +0200

    Fork funcionando

--- Fork.py
+++ Fork.py
@@ -1,14 +1,14 @@
 ### Prueba de Fork
 
 # Sumamos en python!
-num1 = input("Ingresa el primer número: ")
+num1 = input("Ingresa el primer numero: ")
 
-num2 = input("Ingresa el segundo número: ")
+num2 = input("Ingresa el segundo numero: ")
 
 # Convertir las entradas a enteros (int) o flotantes (float) si son decimales
 num1 = float(num1)
 num2 = float(num2)
 
-resultado = num1 - num2
+resultado = num1 + num2
 
-print(f"La suma de {num1} y {num2} es: {resultado}")
+print("La suma de", {num1}," y ",{num2}, "es: ",{resultado})
