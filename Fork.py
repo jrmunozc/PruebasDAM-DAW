99ba2e3a24f82dd39feaf24edfdf1f1b88c8d7b2
José Ramón <munozcolomarjr@gmail.com>
2024-10-01 09:57:35 +0200

    Update and rename Fork.md to Fork.py

---
+++ Fork.py
@@ -0,0 +1,14 @@
+### Prueba de Fork
+
+# Sumamos en python!
+num1 = input("Ingresa el primer número: ")
+
+num2 = input("Ingresa el segundo número: ")
+
+# Convertir las entradas a enteros (int) o flotantes (float) si son decimales
+num1 = float(num1)
+num2 = float(num2)
+
+resultado = num1 - num2
+
+print(f"La suma de {num1} y {num2} es: {resultado}")
